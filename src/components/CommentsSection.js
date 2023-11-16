e240448ec134b57bc45ff02f21832bba606edb7e
Wanessa Lopes <wanessaalopes@gmail.com>
2023-11-16 13:51:24 -0600

    Set buttons padding on mobile

--- src/components/CommentsSection.js
+++ src/components/CommentsSection.js
@@ -1,42 +1,43 @@
 function CommentsSection () {
     return (
-        <article>
-            <div>
-                <img alt=""/>
+      <article>
+        <section>
+          <div className="comments">
+            <div className="comments__division">
+              <div></div>
+              <h4 className="comments__division__name">Micheal Lyons</h4>
+              <p className="comments__division__timestamp">08/09/2021</p>
+              <p className="comments__division__text">
+                They BLEW the ROOF off at their last event, once everyone
+                started figuring out they were going. This is still simply the
+                greatest opening of an event I have EVER witnessed.
+              </p>
             </div>
-            <div>
-                <h2>Join the Conversation</h2>
+            <div className="comments__division">
+              <div></div>
+              <h4 className="comments__division__name">Gary Wong</h4>
+              <p className="comments__division__timestamp">07/15/2021</p>
+              <p className="comments__division__text">
+                Every time I see him shred I feel so motivated to get off my
+                couch and hop on my board. He's so talented! I widh I can ride
+                like him one day so I can really enjoy myself!
+              </p>
             </div>
-            <section>
-            <div>
-                <p>Micheal Lyons</p>
-                <p>08/09/2021</p>
-                <p>
-                They BLEW the ROOF off at their last event, once everyone started
-                figuring out they were going. This is still simply the greatest
-                opening of an event I have EVER witnessed.
-                </p>
+            <div className="comments__division">
+              <div className="comments__divivion__placeholder">
+                <div></div>
+              </div>
+              <h4 className="comments__division__name">Theodore Duncan</h4>
+              <p className="comments__division__timestamp">07/11/2021</p>
+              <p className="comments__division__text">
+                How can someone be so good!!! You can tell he lives this and
+                loves to do it every day. Everytime I see him I feel instantly
+                happy! He's definitely my favorite ever!
+              </p>
             </div>
-            <div>
-                <p>Gary Wong</p>
-                <p>07/15/2021</p>
-                <p>
-                Every time I see him shred I feel so motivated to get off my couch
-                and hop on my board. He's so talented! I widh I can ride like him
-                one day so I can really enjoy myself!
-                </p>
-            </div>
-            <div>
-                <p>Theodore Duncan</p>
-                <p>07/11/2021</p>
-                <p>
-                How can someone be so good!!! You can tell he lives this and loves
-                to do it every day. Everytime I see him I feel instantly happy!
-                He's definitely my favorite ever!
-                </p>
-            </div>
-            </section>
-        </article>
+          </div>
+        </section>
+      </article>
     );
 }
 
