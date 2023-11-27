793ca3b6ee450cbecb4e6d9178bc8c3cb75a1f41
Wanessa Lopes <wanessaalopes@gmail.com>
2023-11-27 12:00:35 -0600

    Set main video player and fonts

--- src/components/MainVideo.js
+++ src/components/MainVideo.js
@@ -3,14 +3,14 @@ import React, { useState } from "react";
 function MainVideo({ video }) {
   return (
     <div className="main__video">
-      <img className="main__video__image" src={video.image} alt=""></img>
-      <video>
+      {/* <img className="main__video__image" src={video.image} alt=""></img> */}
+      <video className="main__video__image" placeholder={video.image} controls>
         <source />
       </video>
       <div className="main__video__headline">
         <h2 className="main__video__headline__title">{video.title}</h2>
       </div>
-      <p>{video.channel}</p>
+      <p className="main__video__channel">{video.channel}</p>
     </div>
   );
 }
