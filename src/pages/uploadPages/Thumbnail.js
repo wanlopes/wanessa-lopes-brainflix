9d74a2cc6d4c1c82f5f5d6942653bcfd2575ee8a
Wanessa Lopes <wanessaalopes@gmail.com>
2023-12-15 14:54:30 -0700

    Add flexbox on comments section

--- src/pages/uploadPages/Thumbnail.js
+++ src/pages/uploadPages/Thumbnail.js
@@ -6,49 +6,57 @@ import upload from "../../assets/Icons/upload.svg"
 function Thumbnail({ handleFormSubmission }) {
   return (
     <div>
-      <div className="thumbnail">
-        <div className="thumbnail__title">
-          <h2 className="thumbnail__title__name">VIDEO THUMBNAIL</h2>
-        </div>
-        <div className="thumbnail__container">
-          <img
-            className="thumbnail__container__image"
-            src={thumbnailimage}
-            alt="thumnailimage"
-          />
-        </div>
-      </div>
-      <div className="form">
-        <div className="form__container">
-          <div className="form__container__title">
-            <h2 className="form__container__title__name">TITLE YOUR VIDEO</h2>
+      <div className="block">
+        <div className="block__thumbnail">
+          <div className="block__thumbnail__title">
+            <h2 className="block__thumbnail__title__name">VIDEO THUMBNAIL</h2>
           </div>
-          <div className="form__container__division">
-            <form className="form__container__division__tag">
-              <input
-                className="form__container__division__tag__placeholder"
-                type="text"
-                placeholder="Add a title to your video"
-              ></input>
-            </form>
+          <div className="block__thumbnail__container">
+            <img
+              className="block__thumbnail__container__image"
+              src={thumbnailimage}
+              alt="thumnailimage"
+            />
           </div>
         </div>
-      </div>
-      <div className="subdivision">
-        <div className="subdivision__title">
-          <h2 className="subdivision__title__name">ADD A VIDEO DESCRIPTION</h2>
-        </div>
-        <div className="subdivision__container">
-          <form
-            className="subdivision__container__form"
-            onSubmit={handleFormSubmission}
-          >
-            <input
-              className="subdivision__container__form__placeholder"
-              type="text"
-              placeholder="Add a description to your video"
-            ></input>
-          </form>
+        <div className="block__wrapper">
+          <div className="block__wrapper__form">
+            <div className="block__wrapper__form__container">
+              <div className="block__wrapper__form__container__title">
+                <h2 className="block__wrapper__form__container__title__name">
+                  TITLE YOUR VIDEO
+                </h2>
+              </div>
+              <div className="block__wrapper__form__container__division">
+                <form className="block__wrapper__form__container__division__tag">
+                  <input
+                    className="block__wrapper__form__container__division__tag__placeholder"
+                    type="text"
+                    placeholder="Add a title to your video"
+                  ></input>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div className="block__wrapper__subdivision">
+            <div className="block__wrapper__subdivision__title">
+              <h2 className="block__wrapper__subdivision__title__name">
+                ADD A VIDEO DESCRIPTION
+              </h2>
+            </div>
+            <div className="block__wrapper__subdivision__container">
+              <form
+                className="block__wrapper__subdivision__container__form"
+                onSubmit={handleFormSubmission}
+              >
+                <input
+                  className="block__wrapper__subdivision__container__form__placeholder"
+                  type="text"
+                  placeholder="Add a description to your video"
+                ></input>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
       <div className="bottom-line"></div>
